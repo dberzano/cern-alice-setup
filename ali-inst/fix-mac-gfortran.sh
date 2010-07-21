97cc1537e91cdc1f263b3c160b97a9bc486d0bff
Dario Berzano <dario.berzano@cern.ch>
2010-07-21 11:47:09 +0000

    * Second, working version of the fixer for gfortran that solves a problem that appeared this week on AliRoot

--- ali-inst/fix-mac-gfortran.sh
+++ ali-inst/fix-mac-gfortran.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Fix function: takes root search directory as the only argument
-function fixMakefile() {
+function FixCommon() {
 
-  #local FIX_SU=`testFixSecondUnderscore`
-  local FIX_SU=1
-  local FIX_DYLIB=1
+  local F
 
   find "$1" -name 'Makefile\.macosx*' -and -not -path '*\.svn*' |
   while read F
@@ -13,52 +11,64 @@ function fixMakefile() {
     egrep "libgfortran.dylib|no-second-underscore" "$F" > /dev/null
     if [ $? == 0 ]; then
       echo -n "Fixing $F..."
-      if [ $FIX_SU == 1 ]; then
-        sed -i '' 's/-fno-second-underscore//' "$F"
-      fi
-      if [ $FIX_DYLIB == 1 ]; then
-        sed -i '' 's/libgfortran\.dylib/libgfortran.a/' "$F"
-      fi
+      sed -i '' 's/-fno-second-underscore//' "$F"
+      sed -i '' 's/libgfortran\.dylib/libgfortran.a/' "$F"
       echo "done"
     fi
   done
 
-  echo "*** Use \"svn revert\" to restore modified files ***"
 }
 
-# Tests if we need to fix the no-second-underscore issue
-function testFixSecondUnderscore() {
-  local T="/tmp/test-fix-su"
+# Specific AliRoot fixes
+function FixAliRoot() {
 
-  cat > "$T.f" <<EOF
-      program hello
-         print *,"Hello World!"
-      end program hello
-EOF
-  gfortran -fno-second-underscore "$T.f" -o "$T.out" #> /dev/null 2>&1
-  local R=$?
-  rm -f "$T.f" "$T.out"
-  if [ $R == 1 ]; then
-    echo 1
-  else
-    echo 0
-  fi
+  local F
+  local DEST
+
+  # -ffree-form introduced in LHAPDF since trunk around r42469
+  find "$1/LHAPDF" \( -name '*Makefile*' -or -name '*.pkg' \) -and \
+    -not -path '*\.svn*' |
+  while read F
+  do
+    egrep -- '-ffree-form|LHpdflib.F' "$F" > /dev/null
+    if [ $? == 0 ]; then
+      echo -n "Fixing $F..."
+      sed -i '' 's/-ffree-form//' "$F"
+      sed -i '' 's/LHpdflib\.F/LHpdflib\.f90/' "$F"
+      echo "done"
+    fi
+  done
+
+  # Create the proper symbolic links
+  find "$1/LHAPDF" -name "LHpdflib.F" |
+  while read F
+  do
+    DEST="`dirname $F`/LHpdflib.f90"
+    echo -n "Creating symlink $DEST..."
+    ln -nfs "$F" "$DEST"
+    echo "done"
+  done
+
 }
 
-# Fix ROOT
+# Fix ROOT?
 if [ "$1" == "--root" ]; then
   if [ -d "$ROOTSYS" ]; then
-    fixMakefile "$ROOTSYS"
+    FixCommon "$ROOTSYS"
   else
     echo "Set \$ROOTSYS to fix ROOT installation." >&2
   fi
-# Fix AliRoot
+# Fix AliRoot?
 elif [ "$1" == "--aliroot" ]; then
   if [ -d "$ALICE_ROOT" ]; then
-    fixMakefile "$ALICE_ROOT"
+    FixCommon "$ALICE_ROOT"
+    FixAliRoot "$ALICE_ROOT"
   else
     echo "Set \$ALICE_ROOT to fix AliRoot installation." >&2
   fi
 else
   echo "Usage: $0 [--root|--aliroot]"
+  exit 1
 fi
+
+echo "*** Use \"svn revert\" to restore modified files ***"
