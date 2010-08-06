fecdedbaf9885931b47b5d86d1f68e7d3542ffe4
Dario Berzano <dario.berzano@cern.ch>
2010-08-06 14:53:41 +0000

    * A better way to display trigger efficiencies

---
+++ app-mtr-eff/macros/DisplayEfficiencies.C
@@ -0,0 +1,109 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// DisplayEfficiencies.C -- by Dario Berzano <dario.berzano@gmail.com>
+//
+// Displays the efficiencies of the muon trigger per local board in the
+// specified OCDB in a simple, uncluttered histogram.
+//
+////////////////////////////////////////////////////////////////////////////////
+void DisplayEfficiencies(TString cdb = "fulleff", Int_t run = 0) {
+
+  AliCDBManager *man = AliCDBManager::Instance();  
+  man->SetDefaultStorage("local://$ALICE_ROOT/OCDB");
+  man->SetSpecificStorage("MUON/Calib/TriggerEfficiency",
+    Form("local://%s/../cdb/%s", gSystem->pwd(), cdb.Data()));
+
+  man->SetRun(run);
+
+  AliCDBEntry *entry;
+  entry = man->Get("MUON/Calib/TriggerEfficiency");
+
+  entry->PrintMetaData();
+  entry->GetId().Print();
+
+  AliMUONTriggerEfficiencyCells *effCells =
+    (AliMUONTriggerEfficiencyCells *)entry->GetObject();
+
+  AliMUONTriggerChamberEfficiency *eff =
+    new AliMUONTriggerChamberEfficiency(effCells);
+
+  // Set my style
+  SetStyle();
+
+  // Canvases
+  TCanvas *cBend = new TCanvas("cBend", "Efficiencies on bending plane", 500,
+    500);
+  cBend->Divide(2, 2);
+
+  TCanvas *cNonBend = new TCanvas("cNonBend",
+    "Efficiencies on nonbending plane", 500, 500);
+  cNonBend->Divide(2, 2);
+
+  TH1F *hBend[4];
+  TH1F *hNonBend[4];
+
+  for (Int_t i=0; i<4; i++) {
+    Int_t detElemId = 1000+100*(i+1);
+
+    hBend[i] = new TH1F(Form("hEffBend_%d", i), Form("Trigger chamber %d", i+1),
+      234, 0.5, 234.5);
+
+    hNonBend[i] = new TH1F(Form("hEffNonBend_%d", i),
+      Form("Trigger chamber %d", i+1), 234, 0.5, 234.5);
+
+    for (Int_t j=1; j<=234; j++) {
+      hBend[i]->SetBinContent(j, eff->GetCellEfficiency(detElemId, j,
+        AliMUONTriggerEfficiencyCells::kBendingEff));
+      hNonBend[i]->SetBinContent(j, eff->GetCellEfficiency(detElemId, j,
+        AliMUONTriggerEfficiencyCells::kBendingEff));
+    }
+
+    cBend->cd(i+1);
+    SetHistoStyle(hBend[i], 0, kBlue, "Local board", "Efficiency", 0., 1.2);
+    hBend[i]->Draw();
+
+    cNonBend->cd(i+1);
+    SetHistoStyle(hNonBend[i], 0, kBlue, "Local board", "Efficiency", 0., 1.2);
+    hNonBend[i]->Draw();
+
+  }
+
+  cBend->cd(0);
+  cNonBend->cd(0);
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Sets the style of the histogram
+////////////////////////////////////////////////////////////////////////////////
+void SetHistoStyle(TH1 *h, Style_t markerStyle, Color_t color,
+  TString xLabel, TString yLabel, Double_t min = 0., Double_t max = 0.,
+  TString title = "", Bool_t stats = kFALSE) {
+
+  h->SetMarkerStyle(markerStyle);
+  if (markerStyle != 0) {
+    h->SetMarkerColor(color);
+  }
+  else {
+    h->SetLineColor(color);
+  }
+  h->SetTitle(title);
+  h->GetXaxis()->SetTitle(xLabel);
+  h->GetYaxis()->SetTitle(yLabel);
+  h->GetYaxis()->SetTitleOffset(1.5);
+  h->SetStats(stats);
+  gPad->SetLeftMargin(0.13);
+
+  if (min != max) {
+    h->SetMinimum(min);
+    h->SetMaximum(max);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Sets the global style of ROOT
+////////////////////////////////////////////////////////////////////////////////
+void SetStyle() {
+  gROOT->SetStyle("Plain");
+  gStyle->SetOptStat("e");
+}
