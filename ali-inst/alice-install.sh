b8c0ba6993119734d550eb40ff547714c74489f4
Dario Berzano <dario.berzano@cern.ch>
2011-02-24 11:43:05 +0000

    * Added progress for CMake build of AliRoot :)

--- ali-inst/alice-install.sh
+++ ali-inst/alice-install.sh
@@ -12,6 +12,8 @@
 #
 
 export SWALLOW_LOG="/tmp/$USER-build-alice"
+export ERR="$SWALLOW_LOG.err"
+export OUT="$SWALLOW_LOG.out"
 export ENVSCRIPT=""
 
 #
@@ -44,55 +46,131 @@ function NiceTime() {
   #printf "%02dh %02dm %02ds" $HH $MM $SS
 }
 
-# Sends everything to a logfile
-function Swallow() {
+# Prints the command name when it is started.
+#  - $1: command description
+function SwallowStart() {
+  local MSG OP CMD
 
-  local ERR="$SWALLOW_LOG.err"
-  local OUT="$SWALLOW_LOG.out"
-  local MSG RET TSSTART TSEND DELTAT
-  local OP="$1"
-  shift
+  OP="$1"
+  CMD="$2"
 
-  MSG='\n*** ['"$(DateTime)"'] BEGIN CWD='"$PWD"' CMD='"$@"' ***\n'
+  MSG='*** ['"$(DateTime)"'] BEGIN CWD='"$PWD"' CMD='"$CMD"' ***'
   echo -e "$MSG" >> "$OUT"
   echo -e "$MSG" >> "$ERR"
 
   echo -en "[....] $OP..."
-  TSSTART=$(date +%s)
-  "$@" >> "$OUT" 2>> "$ERR"
-  RET=$?
-  TSEND=$(date +%s)
 
-  let DELTAT=TSEND-TSSTART
+}
 
+# Prints command's progress with percentage and time.
+#  - $1: command description
+#  - $2: current percentage
+#  - $3: start timestamp (seconds)
+function SwallowStep() {
+  local TS_START OP MSG PCT PCT_FMT
+
+  OP="$1"
+  PCT=$2
+  TS_START=${3}
+
+  let TS_DELTA=$(date +%s)-TS_START
+
+  # Prints progress
+  echo -ne '\r                                                                                               \r'
+  PCT_FMT=$( printf "%3d%%" $PCT )
+  echo -ne "[\033[1;34m$PCT_FMT\033[m] $OP \033[1;36m$(NiceTime $TS_DELTA)\033[m"
+
+  return $RET
+
+}
+
+# Prints the command with its exit status (OK or FAILED) and time taken.
+#  - $1: command description
+#  - $2: the exit code of command
+#  - $3: start timestamp (seconds) (optional)
+#  - $4: end timestamp (seconds) (optional)
+function SwallowEnd() {
+
+  local TS_END TS_START OP MSG RET
+
+  OP="$1"
+  RET=$2
+  TS_START=${3-0}  # defaults to 0
+  TS_END=${4-0}
+
+  let TS_DELTA=TS_END-TS_START
+
+  # Prints success (green OK) or fail (red FAIL)
   echo -ne '\r'
   [ $RET == 0 ] && \
-    echo -en '[ \033[1;32mOK\033[m ]' || echo -en '[\033[1;31mFAIL\033[m]'
-  echo -e " $OP \033[1;36m$(NiceTime $DELTAT)\033[m"
+    echo -ne '[ \033[1;32mOK\033[m ]' || echo -ne '[\033[1;31mFAIL\033[m]'
+  echo -ne " ${OP}"
 
-  MSG='\n*** ['"$(DateTime)"'] END CWD='"$PWD"' ERR='"$RET"' CMD='"$@"' ***\n'
+  # Prints time only if greater than 1 second
+  if [ $TS_DELTA -gt 1 ]; then
+    echo -e " \033[1;36m$(NiceTime $TS_DELTA)\033[m"
+  else
+    echo "   "
+  fi
+
+  # On the log files (out, err)
+  MSG='*** ['"$(DateTime)"'] END CWD='"$PWD"' ERR='"$RET"' CMD='"$@"' ***'
   echo -e "$MSG" >> "$OUT"
   echo -e "$MSG" >> "$ERR"
 
   return $RET
 }
 
-# Like Swallow, but terminates on fatal error
-function SwallowFatal() {
-  local LASTLINES=20
-  Swallow "$@"
-  if [ $? != 0 ]; then
-    echo ""
-    echo -e "\033[1;41m\033[1;37m!!! Operation $1 ended with errors !!!\033[m"
-    echo ""
-    echo -e "\033[1;33m=== Last $LASTLINES lines of stdout -- $SWALLOW_LOG.out ===\033[m"
-    tail -n$LASTLINES "$SWALLOW_LOG".out
-    echo ""
-    echo -e "\033[1;33m=== Last $LASTLINES lines of stderr -- $SWALLOW_LOG.err ===\033[m"
-    tail -n$LASTLINES "$SWALLOW_LOG".err
-    echo ""
+# Sends everything to a logfile
+function Swallow() {
+
+  local MSG RET TSSTART TSEND DELTAT FATAL OP
+
+  # Abort on errors?
+  if [ "$1" == "-f" ]; then
+    FATAL=1
+    shift
+  else
+    FATAL=0
+  fi
+
+  OP="$1"
+  shift
+
+  SwallowStart "$OP" "$@"
+  TSSTART=$(date +%s)
+
+  "$@" >> "$OUT" 2>> "$ERR"
+  RET=$?
+
+  TSEND=$(date +%s)
+  SwallowEnd "$OP" $RET $TSSTART $TSEND
+
+  if [ $RET != 0 ] && [ $FATAL == 1 ]; then
+    LastLogLines -e
     exit 1
   fi
+
+  return $RET
+}
+
+# Prints the last lines of both log files
+function LastLogLines() {
+  local LASTLINES=20
+  local ISERROR=0
+
+  if [ "$1" == "-e" ]; then
+    echo ""
+    echo -e "\033[1;41m\033[1;37m!!! Operation $1 ended with errors !!!\033[m"
+  fi
+
+  echo ""
+  echo -e "\033[1;33m=== Last $LASTLINES lines of stdout -- $SWALLOW_LOG.out ===\033[m"
+  tail -n$LASTLINES "$SWALLOW_LOG".out
+  echo ""
+  echo -e "\033[1;33m=== Last $LASTLINES lines of stderr -- $SWALLOW_LOG.err ===\033[m"
+  tail -n$LASTLINES "$SWALLOW_LOG".err
+  echo ""
 }
 
 # Echoes a colored banner
@@ -103,22 +181,22 @@ function Banner() {
 # Module to fetch and compile ROOT
 function ModuleRoot() {
   Banner "Compiling ROOT..."
-  SwallowFatal "Sourcing envvars" source "$ENVSCRIPT" -n
+  Swallow -f "Sourcing envvars" source "$ENVSCRIPT" -n
 
   if [ ! -d "$ROOTSYS" ]; then
-    SwallowFatal "Creating ROOT directory" mkdir -p "$ROOTSYS"
+    Swallow -f "Creating ROOT directory" mkdir -p "$ROOTSYS"
   fi
 
-  SwallowFatal "Moving into ROOT directory" cd "$ROOTSYS"
+  Swallow -f "Moving into ROOT directory" cd "$ROOTSYS"
 
   if [ ! -e $ROOTSYS/configure ]; then
     [ $ROOT_VER == "trunk" ] && \
       CMD="svn co https://root.cern.ch/svn/root/trunk ." || \
       CMD="svn co https://root.cern.ch/svn/root/tags/$ROOT_VER ."
-    SwallowFatal "Downloading ROOT $ROOT_VER" $CMD
+    Swallow -f "Downloading ROOT $ROOT_VER" $CMD
   fi
 
-  SwallowFatal "Configuring ROOT" ./configure \
+  Swallow -f "Configuring ROOT" ./configure \
     --with-pythia6-uscore=SINGLE \
     --with-alien-incdir="$GSHELL_ROOT/include" \
     --with-alien-libdir="$GSHELL_ROOT/lib" \
@@ -127,67 +205,119 @@ function ModuleRoot() {
     --enable-minuit2 \
     --enable-roofit \
     --enable-soversion
-  SwallowFatal "Building ROOT" make -j$MJ
+  Swallow -f "Building ROOT" make -j$MJ
 }
 
 # Module to fetch and compile Geant3
 function ModuleGeant3() {
   Banner "Compiling Geant3..."
-  SwallowFatal "Sourcing envvars" source "$ENVSCRIPT" -n
+  Swallow -f "Sourcing envvars" source "$ENVSCRIPT" -n
 
   if [ ! -d "$GEANT3DIR" ]; then
-    SwallowFatal "Creating Geant3 directory" mkdir -p "$GEANT3DIR"
+    Swallow -f "Creating Geant3 directory" mkdir -p "$GEANT3DIR"
   fi
 
-  SwallowFatal "Moving into Geant3 directory" cd "$GEANT3DIR"
+  Swallow -f "Moving into Geant3 directory" cd "$GEANT3DIR"
 
   if [ ! -e make ]; then
     [ $G3_VER == "trunk" ] && \
       CMD="svn co https://root.cern.ch/svn/geant3/trunk ." || \
       CMD="svn co https://root.cern.ch/svn/geant3/tags/$G3_VER ."
-    SwallowFatal "Downloading Geant3 $G3_VER" $CMD
+    Swallow -f "Downloading Geant3 $G3_VER" $CMD
   fi
 
-  SwallowFatal "Building Geant3" make
+  Swallow -f "Building Geant3" make
 }
 
 # Module to fetch, update and compile AliRoot
 function ModuleAliRoot() {
   Banner "Compiling AliRoot..."
-  SwallowFatal "Sourcing envvars" source "$ENVSCRIPT" -n
+  Swallow -f "Sourcing envvars" source "$ENVSCRIPT" -n
 
   if [ ! -d "$ALICE_ROOT" ]; then
-    SwallowFatal "Creating AliRoot source directory" mkdir -p "$ALICE_ROOT"
+    Swallow -f "Creating AliRoot source directory" mkdir -p "$ALICE_ROOT"
   fi
 
   if [ ! -d "$ALICE_INSTALL" ]; then
-    SwallowFatal "Creating AliRoot build directory" mkdir -p "$ALICE_INSTALL"
+    Swallow -f "Creating AliRoot build directory" mkdir -p "$ALICE_INSTALL"
   fi
 
-  SwallowFatal "Moving into AliRoot source directory" cd "$ALICE_ROOT"
+  Swallow -f "Moving into AliRoot source directory" cd "$ALICE_ROOT"
 
   if [ ! -d "STEER" ]; then
     [ "$ALICE_VER" == "trunk" ] && \
       CMD="svn co https://alisoft.cern.ch/AliRoot/trunk ." || \
       CMD="svn co https://alisoft.cern.ch/AliRoot/tags/$ALICE_VER ."
-    SwallowFatal "Downloading AliRoot $ALICE_VER" $CMD
+    Swallow -f "Downloading AliRoot $ALICE_VER" $CMD
   fi
 
-  SwallowFatal "Updating AliRoot" svn up
-  SwallowFatal "Moving into AliRoot build directory" cd "$ALICE_INSTALL"
+  Swallow -f "Updating AliRoot" svn up
+  Swallow -f "Moving into AliRoot build directory" cd "$ALICE_INSTALL"
 
   if [ ! -e "Makefile" ]; then
-    SwallowFatal "Bootstrapping AliRoot build with cmake" cmake "$ALICE_ROOT"
+    Swallow -f "Bootstrapping AliRoot build with cmake" cmake "$ALICE_ROOT"
   fi
 
-  SwallowFatal "Building AliRoot" make -j$MJ
+  SwallowProgress -f "Building AliRoot" make -j$MJ
 
-  SwallowFatal "Symlinking AliRoot include directory" \
+  Swallow -f "Symlinking AliRoot include directory" \
     ln -nfs "$ALICE_INSTALL"/include "$ALICE_ROOT"/include
 
-  SwallowFatal "Sourcing envvars" source "$ENVSCRIPT" -n
-  SwallowFatal "Testing ROOT with AliRoot libraries" \
+  Swallow -f "Sourcing envvars" source "$ENVSCRIPT" -n
+  Swallow -f "Testing ROOT with AliRoot libraries" \
     root -l -q "$ALICE_ROOT"/macros/loadlibs.C
+
+}
+
+# Cmake progress
+function SwallowProgress() {
+
+  local BGPID PCT PCT_FMT OP FATAL
+
+  # Abort on error?
+  if [ "$1" == "-f" ]; then
+    FATAL=1
+    shift
+  else
+    FATAL=0
+  fi
+
+  OP="$1"
+  shift
+
+  SwallowStart "$OP" "$@"
+  TSSTART=$(date +%s)
+
+  #( $@ > "$SWALLOW_LOG".out 2> "$SWALLOW_LOG".err ) &
+  "$@" >> "$OUT" 2>> "$ERR" &
+  BGPID=$!
+
+  while ps|grep -v grep|grep $BGPID > /dev/null 2>&1
+  do
+
+    # Parse current percentage
+    PCT=$( grep --text '%]' "$OUT" | \
+      perl -ne '/\[\s*([0-9]{1,3})%\]/; print "$1\n"' | tail -n1 )
+
+    # Show progress
+    SwallowStep "$OP" "$PCT" $TSSTART
+
+    # Sleep
+    sleep 1
+
+  done
+
+  # It has finished: check exitcode
+  wait $BGPID
+  RET=$?
+
+  TSEND=$(date +%s)
+  SwallowEnd "$OP" $RET $TSSTART $TSEND
+
+  if [ $RET != 0 ] && [ $FATAL == 1 ]; then
+    LastLogLines -e
+    exit 1
+  fi
 
 }
 
@@ -207,12 +337,12 @@ function Dl() {
 function ModuleAliEn() {
   local ALIEN_INSTALLER="/tmp/alien-installer-$USER"
   Banner "Installing AliEn..."
-  SwallowFatal "Sourcing envvars" source "$ENVSCRIPT" -n
-  SwallowFatal "Downloading AliEn installer" \
+  Swallow -f "Sourcing envvars" source "$ENVSCRIPT" -n
+  Swallow -f "Downloading AliEn installer" \
     Dl http://alien.cern.ch/alien-installer "$ALIEN_INSTALLER"
-  SwallowFatal "Making AliEn installer executable" \
+  Swallow -f "Making AliEn installer executable" \
     chmod +x "$ALIEN_INSTALLER"
-  SwallowFatal "Installing AliEn" \
+  Swallow -f "Installing AliEn" \
     "$ALIEN_INSTALLER" -install-dir "$ALIEN_DIR" -batch -notorrent
   rm -f "$ALIEN_INSTALLER"
 }
@@ -221,15 +351,15 @@ function ModuleAliEn() {
 function ModulePrepare() {
   local TF
   Banner "Creating directory structure..."
-  SwallowFatal "Sourcing envvars" source "$ENVSCRIPT" -n
+  Swallow -f "Sourcing envvars" source "$ENVSCRIPT" -n
 
   mkdir -p "$ALICE_PREFIX" 2> /dev/null
 
   if [ "$USER" == "root" ]; then
-    SwallowFatal "Creating ALICE software directory" [ -d "$ALICE_PREFIX" ]
-    SwallowFatal "Opening permissions of ALICE directory" \
+    Swallow -f "Creating ALICE software directory" [ -d "$ALICE_PREFIX" ]
+    Swallow -f "Opening permissions of ALICE directory" \
       chmod 0777 "$ALICE_PREFIX"
-    #SwallowFatal ""
+    #Swallow -f ""
   else
     TF="$ALICE_PREFIX/dummy_$RANDOM"
     touch "$TF" 2> /dev/null
