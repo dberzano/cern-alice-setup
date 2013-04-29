e520ca813ecf20ca249a30756f298f3555abf49a
Dario Berzano <dario.berzano@cern.ch>
2013-04-29 16:01:09 +0200

    Converted to Git: changed directory structure

--- app-mtr-eff/simrec/jobstat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#watch -n1 'echo -n "Jobs running: " ; qstat | cut -b69-69 | sort | egrep -v -- "[-S]" | grep -c R ; echo -n "Jobs queued:  " ; qstat | cut -b69-69 | sort | grep -c Q ;'
-watch -n1 'showq|tail -n1'
--- app-mtr-eff/simrec/rmzero.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find . -size 0 -maxdepth 1 -exec rm -f '{}' \;
