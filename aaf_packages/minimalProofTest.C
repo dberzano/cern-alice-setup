55f6d81cf22d433e87de7f870088379873bc9b20
Dario Berzano <dario.berzano@cern.ch>
2015-01-30 12:13:43 +0100

    AliPar: test macro supports AliPhysics

--- aaf_packages/minimalProofTest.C
+++ aaf_packages/minimalProofTest.C
@@ -1,12 +1,20 @@
 void minimalProofTest() {
-  //TString aliRootVer = "vAN-20141201";
-  //TString aliRootVer = "vAN-20150107";
-  TString aliRootVer = "v5-06-02";
+
+  // TString swName = "AliRoot";
+  // //TString swVer = "vAN-20141201";
+  // //TString swVer = "vAN-20150107";
+  // TString swVer = "v5-06-02";
+
+  TString swName = "AliPhysics";
+  //TString swVer = "vAN-20141201";
+  //TString swVer = "vAN-20150107";
+  TString swVer = "vAN-20150129";
+
   TString rootVer = "v5-34-08-6";
   TString connStr = "dberzano@alice-caf.cern.ch";
 
   gSystem->Exec( "rm -f VO*.par 2> /dev/null" );
-  gSystem->Exec( Form("./gen_single_par.sh 'VO_ALICE@AliRoot::%s'", aliRootVer.Data()) );
+  gSystem->Exec( Form("./gen_single_par.sh 'VO_ALICE@%s::%s'", swName.Data(), swVer.Data()) );
 
   TProof::Reset(connStr.Data());
   TProof::Mgr(connStr.Data())->SetROOTVersion(Form("VO_ALICE@ROOT::%s", rootVer.Data()));
@@ -14,12 +22,18 @@ void minimalProofTest() {
   if (!gProof) return;
 
   gProof->ClearPackages();
-  gProof->UploadPackage( Form("VO_ALICE@AliRoot::%s", aliRootVer.Data()) );
-  gProof->EnablePackage( Form("VO_ALICE@AliRoot::%s", aliRootVer.Data()) );
+  gProof->UploadPackage( Form("VO_ALICE@%s::%s", swName.Data(), swVer.Data()) );
+  gProof->EnablePackage( Form("VO_ALICE@%s::%s", swName.Data(), swVer.Data()) );
 
-  // Print some tests
-  Printf("What is ALICE_ROOT on the remote node?");
+  // Run some tests
+  Printf("* What is ALICE_ROOT on the remote node?");
   gProof->Exec( ".!echo $ALICE_ROOT" );
-  Printf("What is ALICE_PHYSICS on the remote node?");
+  Printf("* What is ALICE_PHYSICS on the remote node?");
   gProof->Exec( ".!echo $ALICE_PHYSICS" );
+  Printf("* What is the library path?");
+  gProof->Exec( "cout << gSystem->GetDynamicPath() << endl;" );
+  Printf("* What is the include path?");
+  gProof->Exec( "cout << gSystem->GetIncludePath() << endl;" );
+  Printf("* What is the macro path?");
+  gProof->Exec( "cout << gROOT->GetMacroPath() << endl;" );
 }
