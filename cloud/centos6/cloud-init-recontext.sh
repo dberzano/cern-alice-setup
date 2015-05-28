06ce3df7f41088e96d4c19e16844742347755e41
Dario Berzano <dario.berzano@cern.ch>
2015-05-28 10:38:02 +0200

    CentOS: recontext sets NoCloud as datasource

--- cloud/centos6/cloud-init-recontext.sh
+++ cloud/centos6/cloud-init-recontext.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-# Note: use only NoCloud as data source
+
+# Echo every command, and make every command fatal
+set -x
+set -e
+
+# Use NoCloud as data source
+cat /etc/cloud/cloud.cfg | \
+  sed -e 's|^.*datasource_list:.*$|datasource_list: [ '\''NoCloud'\'' ]|' > \
+  /etc/cloud/cloud.cfg.0 && mv /etc/cloud/cloud.cfg.0 /etc/cloud/cloud.cfg
+
+### Custom cleanup ###
+true
+### /Custom cleanup ###
+
+# Remove data from previous runs
 rm -fr /var/lib/cloud/instance/*
+
+# Create the directory where the NoCloud module looks for user-data and meta-data
 mkdir -p /var/lib/cloud/seed/nocloud-net || exit 1
+
+# Make our user-data visible to the NoCloud module (copy)
 cp -v /home/cloud-user/cloud-init.txt /var/lib/cloud/seed/nocloud-net/user-data || exit 1
+
+# Create an empty meta-data (without meta-data, it will fail)
 echo '' > /var/lib/cloud/seed/nocloud-net/meta-data
+
+# Execute the cloud-init contextualization phases in order
 service cloud-init-local restart
 service cloud-init restart
 service cloud-config restart
