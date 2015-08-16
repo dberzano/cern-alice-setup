eec012ab4cb5dcda1a0e399bf9f487ea38b77104
Dario Berzano <dario.berzano@cern.ch>
2015-08-16 13:01:31 +0200

    CVMFS: added Slack notification

--- cvmfs-check-todays-tag.sh
+++ cvmfs-check-todays-tag.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-# Send an email if AliPhysics is not yet on CVMFS. Use in crontab at ~18.30
+
+# Send an email if AliPhysics is not yet on CVMFS. Use in crontab at ~18.30.
+# Always send a Slack notification.
+
+source "$(echo "$0" | sed -e 's|\.sh$|.config|')" || exit 3
 Pref='/cvmfs/alice.cern.ch/x86_64-2.6-gnu-4.1.2/Packages/AliPhysics'
 AnTag=`env TZ=Europe/Zurich LANG=C date --date='now' +vAN-%Y%m%d`
-[[ -d $Pref/$AnTag ]] && exit 0
-cat | mailx -r cvmfs-checker \
-            -s "[CVMFS Checker] $AnTag still not available" \
-            root <<EoF
+if [[ -d $Pref/$AnTag ]]; then
+  echo "OK: $Pref/$AnTag" >&2
+  SlackMsg="Today's AliPhysics tag *$AnTag* is available on CVMFS :thumbsup:"
+  SlackColor="good"
+  rv=0
+else
+  echo "Not OK, sending mail: $Pref/$AnTag" >&2
+  mailx -r cvmfs-checker \
+              -s "[CVMFS Checker] $AnTag still not available" \
+              root <<EoF
 Today's AliPhysics tag ($AnTag) is not available on CVMFS:
 
   $Pref/$AnTag
@@ -14,3 +24,29 @@ Please check.
 --
 CVMFS Checker
 EoF
+  SlackMsg="Today's AliPhysics tag *$AnTag* is not yet available on CVMFS :scream: Could you please have a look?"
+  SlackColor="danger"
+  rv=2
+fi
+
+SlackPayload=$(cat <<EoF
+payload={
+  "channel": "$SlackChannel",
+  "username": "cvmfs-checker",
+  "text": "$SlackMsg",
+  "attachments": [{
+    "pretext": "Path:",
+    "text": "\`$Pref/$AnTag\`",
+    "color": "$SlackColor",
+    "mrkdwn_in": [ "text" ]
+   }]
+}
+EoF
+)
+
+curl -L -X POST \
+     --silent \
+     --data "$SlackPayload" \
+     "$SlackUrl" > /dev/null
+
+exit $rv
